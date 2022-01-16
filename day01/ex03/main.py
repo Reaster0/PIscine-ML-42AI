7213d7dcbe6484ef545ef31c1b27f58ee330edf1
Evan Reaster <reaster@MacBook-Reaster.local>
2022-01-16 18:11:02 +0100

    ex03 i suppose is done

---
+++ day01/ex03/main.py
@@ -0,0 +1,17 @@
+import numpy as np
+from my_linear_regression import MyLinearRegression as MyLR
+x = np.array([[12.4956442], [21.5007972], [31.5527382], [48.9145838], [57.5088733]])
+y = np.array([[37.4013816], [36.1473236], [45.7655287], [46.6793434], [59.5585554]])
+lr1 = MyLR([2, 0.7])
+
+#print(lr1.predict_(x))
+#print(lr1.loss_elem_(lr1.predict_(x),y))
+#print(lr1.loss_(lr1.predict_(x),y))
+
+lr2 = MyLR([[1], [1]], 5e-8, 1500000)
+lr2.fit_(x, y)
+#print(lr2.thetas)
+
+#print(lr2.predict_(x))
+print(MyLR.loss_elem_(lr2.predict_(x), y))
+print(MyLR.loss_(lr2.predict_(x), y))
---
+++ day01/ex03/my_linear_regression.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class MyLinearRegression():
+	def __init__(self, thetas, alpha=0.001, max_iter=1000):
+		self.alpha = alpha
+		self.max_iter = max_iter
+		self.thetas = thetas
+	def loss_elem_(y, y_hat):
+		result = []
+		for index, i in enumerate(y):
+			result.append(pow(y_hat[index] - i, 2))
+		return result
+	def loss_(y, y_hat):
+		result = 0
+		for index, i in enumerate(y):
+			result += pow(y_hat[index] - i, 2)
+		return result / (2 * len(y_hat))
+	def predict_(self, x):
+		new_row = np.ones(len(x), dtype=np.int8)
+		result = np.dot(np.hstack((np.transpose([new_row]), x)), self.thetas)
+		return result
+	def gradient_(self, x, y):
+		temp = np.ones((len(x), 1))
+		x_prim = np.hstack((temp, x))
+		return np.divide(np.matmul(x_prim.T, np.subtract(np.matmul(x_prim, self.thetas), y)), len(y))
+	def fit_(self, x, y):
+		for i in range(self.max_iter):
+			temp = self.gradient_(x, y)
+			self.thetas = np.subtract(self.thetas, np.multiply(temp, self.alpha))
